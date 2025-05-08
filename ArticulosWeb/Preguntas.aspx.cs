e2d063550c4d25de14619e400be86e990f7959a2
agust <agustin198ar@gmail.com>
2025-05-08 16:13:07 -0300

    update 2 8/5 fix emailservice

--- ArticulosWeb/Preguntas.aspx.cs
+++ ArticulosWeb/Preguntas.aspx.cs
@@ -19,21 +19,20 @@ namespace ArticulosWeb
         {
             EmailService emailService = new EmailService();
             emailService.armarCorreo(txtEmail.Text, txtAsunto.Text, txtMensaje.Text);
-            try
-            {
-                emailService.enviarEmail();
-            }
-            catch (Exception ex)
-            {
-                Session.Add("error", ex);
-            }
-
-
             lblEnviar.Text = "Gracias por enviarnos mensaje, en breve solucionaremos tus dudas😊";
             lblEnviar.Visible = true;
             // Agregar un script para ocultar el label después de 4 segundos
             string script = "<script>setTimeout(function() { document.getElementById('" + lblEnviar.ClientID + "').style.display = 'none'; }, 5000);</script>";
             ClientScript.RegisterStartupScript(this.GetType(), "HideLabel", script);
+            try
+            {
+                emailService.enviarEmail();
+
+            }
+            catch (Exception ex)
+            {
+                Session.Add("error", ex);
+            }
         }
     }
 }
